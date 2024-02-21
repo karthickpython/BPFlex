2523e885c6ca305882795fd4a29ca4eda504fd7f
karthickpython <jet.karthick@rocketmail.com>
2024-02-21 11:09:25 +0530

    Your commit message

---
+++ libbpflex/sslsniff.h
@@ -0,0 +1,21 @@
+#ifndef __SSLSNIFF_H
+#define __SSLSNIFF_H
+
+#define MAX_BUF_SIZE 8192
+#define TASK_COMM_LEN 16
+
+struct probe_SSL_data_t {
+    __u64 timestamp_ns;
+    __u64 delta_ns;
+    __u32 pid;
+    __u32 tid;
+    __u32 uid;
+    __u32 len;
+    int buf_filled;
+    int rw;
+    char comm[TASK_COMM_LEN];
+    __u8 buf[MAX_BUF_SIZE];
+    int is_handshake;
+};
+
+#endif /* __SSLSNIFF_H */
